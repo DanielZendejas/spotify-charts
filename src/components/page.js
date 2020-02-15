e877384fdf46b31c84b67b45681fb2c01973801b
DanielZendejas <daniel8647@hotmail.com>
2020-02-15 17:25:36 -0600

    Add title and description to page

--- src/components/page.js
+++ src/components/page.js
@@ -84,6 +84,12 @@ class Page extends React.Component {
           class="container"
           style={{"display":"flex-wrap"}}
         >
+          <h1 class="text-center" style={{"color":"#bbe1fa"}}>
+            Album Charts
+          </h1>
+          <p class="text-center" style={{"color":"#bbe1fa"}}>
+            Shows your most recently listened albums. Select the dimensions for your chart and then download it as an image.
+          </p>
           <Chart
             covers={data.covers}
             coversGridWidth={this.state.coversGridWidth}
